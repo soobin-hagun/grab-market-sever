a206b034a3990f6c7da7a692f38942508e7b11e1
github KwonSoobin <github soobin@hagunlab.com>
2022-02-11 15:21:06 +0900

    hiroku배포준비

--- index.js
+++ index.js
@@ -1,25 +1,142 @@
-var http = require('http'); // node 내장 모듈 불러옴 
-var hostname = '127.0.0.1'; // localhost와 동일 
-var port = 8080; 
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+const port = process.env.PORT || 8082;
 
-const server = http.createServer(function(req, res) { 
-    const path = req.url; //req.url에는 port와 ip번호를 제외한 url이 들어가게 됩니다.
-    const method = req.method;
-    if(path ==='/products'){ //아티클 정보를 받아오는 요청
-        if(req.method === 'GET'){ //멤버들 정보를 받아오는 요청
-            res.writeHead(200,{'Content-Type': 'application/json'});
-            const products = JSON.stringify([{
-                name : "농구공",
-                price : 5000
-            }]);
-            res.end(products);
-        } 
-        
-        else if(method === 'POST'){
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+//배너를 주는 API만들기
+app.use("/banners", (req,res) =>{
+  models.Banner.findAll({
+    limit:2
+  }).then((result)=>{
+    res.send({
+      banners: result,
+    });
+  }).catch((error)=>{
+    console.error(error);
+    res.status(500).send('에러가 발생했습니다');    
+  });
+})
 
-        } //멤버가 회원가입을 할 때 요청
+app.get("/products", (req, res) => {
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl","soldout"],
+  })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("에러 발생");
+    });
+});
 
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+    imageUrl,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다");
+    });
+});
+
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 조회에 에러가 발생했습니다");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
+
+app.post("/purchase/:id", (req, res) => {
+  const { id } = req.params;
+  models.Product.update(
+    {
+      soldout: 1,
+    },
+    {
+      where: {
+        id,
+      },
     }
+  )
+    .then((result) => {
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
 
-    res.end("Good Bye");
+
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러ㅠ");
+      process.exit();
+    });
 });
